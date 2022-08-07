faa48c91a01fad76aed215a371b03f2714b407f6
Todd Garner <tod2garner@gmail.com>
2022-08-06 22:07:44 -0400

    wip: partial work to start on aesthetics

---
+++ GeneratorEngine/Flavor/Aesthetic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeneratorEngine.Flavor
+{
+    public class Aesthetic
+    {
+        public string DeliveryDescription;// (ex: jet, beam, ball)
+        public string Color;
+        public string MaterialDescription;// (ex: burning, swirling, writhing)
+        public string MaterialType;
+
+        public string CombinedDescription => $"{DeliveryDescription} of {Color}, {MaterialDescription} {MaterialType}";
+
+        //TODO - move to generator
+
+        //DeliveryTypes
+        //   Combine with range types? Self, Melee, Ranged
+        /*
+           Touch
+           None
+           AreaOfEffect
+           Projectile
+           AreaProjectile
+           Weapon
+         * 
+         */
+    }
+}
