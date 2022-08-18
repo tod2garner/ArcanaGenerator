aee6785d768862b8a4ce5757a5fc6b68e721af7b
Todd Garner <tod2garner@gmail.com>
2022-08-18 11:44:50 -0400

    fix: correcting typo with AoEProjectile desciption

--- GeneratorEngine/AreaProjectileDelivery.cs
+++ GeneratorEngine/AreaProjectileDelivery.cs
@@ -18,8 +18,8 @@ namespace GeneratorEngine
         internal override void UpdateDescription()
         {
             base.UpdateDescription();
-            Description = Description.Replace("Projectile", $"{ProjectileType} Projectile");
-            Description += $" and an {Area.Size} ft {Area.Shape} area";
+            Description = Description.Replace("AreaProjectile", $"{ProjectileType} AreaProjectile");
+            Description += $" and an {Area.Size} ft {Area.Shape} area of effect";
         }
 
         internal override void ScalePower(double? scalingRatio)
